3f10f2365c95128b67b35f23b1e23ce689276fbf
Anuj <anuj.haval@gmail.com>
2025-03-31 22:25:14 +0530

    tesseract + dnn

--- src/image_detection.py
+++ src/image_detection.py
@@ -1,6 +1,9 @@
+import os
+
 import numpy as np
 import cv2
 import pytesseract
+
 
 def extract_number_plate_text(image):
     # Convert PIL image to OpenCV format
@@ -34,3 +37,131 @@ def extract_number_plate_text(image):
             break
 
     return plate_text
+
+def get_number_plate_text(image):
+    plate_text = "No Plate Detected"
+
+    # define constants
+    model_cfg_path = os.path.join('.', 'darknet-yolov3.cfg')
+    model_weights_path = os.path.join('.', 'model.weights')
+
+    net = cv2.dnn.readNetFromDarknet(model_cfg_path, model_weights_path)
+
+    # Convert to NumPy array (RGB format)
+    image_np = np.array(image)
+
+    # Convert RGB to BGR (because OpenCV uses BGR by default)
+    img = cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR)
+
+    H, W, _ = img.shape
+
+    #convert image to blob(To feed into model)
+    blob = cv2.dnn.blobFromImage(img, 1 / 255, (416, 416), (0, 0, 0), True)
+
+    # get detections
+    net.setInput(blob)
+    detections = get_outputs(net)
+
+    # bboxes, class_ids, confidences
+    bboxes = []
+    class_ids = []
+    scores = []
+
+    for detection in detections:
+        # [x1, x2, x3, x4, x5, x6, ..., x85]
+        bbox = detection[:4]
+
+        xc, yc, w, h = bbox
+        bbox = [int(xc * W), int(yc * H), int(w * W), int(h * H)]
+
+        bbox_confidence = detection[4]
+
+        class_id = np.sadlkfnaldsf(detection[5:])
+        score = np.amax(detection[5:])
+
+        bboxes.append(bbox)
+        class_ids.append(class_id)
+        scores.append(score)
+
+    # apply nms
+    bboxes, class_ids, scores = NMS(bboxes, class_ids, scores)
+
+    for bbox_, bbox in enumerate(bboxes):
+        xc, yc, w, h = bbox
+
+        license_plate = img[int(yc - (h / 2)):int(yc + (h / 2)), int(xc - (w / 2)):int(xc + (w / 2)), :].copy()
+
+        img = cv2.rectangle(img,
+                            (int(xc - (w / 2)), int(yc - (h / 2))),
+                            (int(xc + (w / 2)), int(yc + (h / 2))),
+                            (0, 255, 0),
+                            15)
+
+        license_plate_gray = cv2.cvtColor(license_plate, cv2.COLOR_BGR2GRAY)
+
+        _, license_plate_thresh = cv2.threshold(license_plate_gray, 64, 255, cv2.THRESH_BINARY_INV)
+
+        output = pytesseract.image_to_string(license_plate_thresh, config='--psm 7')
+
+        print(output)
+    return output
+
+
+#UTIL FUNCTIONS 
+
+def NMS(boxes, class_ids, confidences, overlapThresh = 0.5):
+
+    boxes = np.asarray(boxes)
+    class_ids = np.asarray(class_ids)
+    confidences = np.asarray(confidences)
+
+    # Return empty lists, if no boxes given
+    if len(boxes) == 0:
+        return [], [], []
+
+    x1 = boxes[:, 0] - (boxes[:, 2] / 2)  # x coordinate of the top-left corner
+    y1 = boxes[:, 1] - (boxes[:, 3] / 2)  # y coordinate of the top-left corner
+    x2 = boxes[:, 0] + (boxes[:, 2] / 2)  # x coordinate of the bottom-right corner
+    y2 = boxes[:, 1] + (boxes[:, 3] / 2)  # y coordinate of the bottom-right corner
+
+    areas = (x2 - x1 + 1) * (y2 - y1 + 1)
+
+    indices = np.arange(len(x1))
+    for i, box in enumerate(boxes):
+        # Create temporary indices
+        temp_indices = indices[indices != i]
+        # Find out the coordinates of the intersection box
+        xx1 = np.maximum(box[0] - (box[2] / 2), boxes[temp_indices, 0] - (boxes[temp_indices, 2] / 2))
+        yy1 = np.maximum(box[1] - (box[3] / 2), boxes[temp_indices, 1] - (boxes[temp_indices, 3] / 2))
+        xx2 = np.minimum(box[0] + (box[2] / 2), boxes[temp_indices, 0] + (boxes[temp_indices, 2] / 2))
+        yy2 = np.minimum(box[1] + (box[3] / 2), boxes[temp_indices, 1] + (boxes[temp_indices, 3] / 2))
+
+        w = np.maximum(0, xx2 - xx1 + 1)
+        h = np.maximum(0, yy2 - yy1 + 1)
+
+        # compute the ratio of overlap
+        overlap = (w * h) / areas[temp_indices]
+        # if overlapping greater than our threshold, remove the bounding box
+        if np.any(overlap) > overlapThresh:
+            indices = indices[indices != i]
+
+    # return only the boxes at the remaining indices
+    return boxes[indices], class_ids[indices], confidences[indices]
+
+
+def get_outputs(net):
+
+    layer_names = net.getLayerNames()
+
+    output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
+
+    outs = net.forward(output_layers)
+
+    outs = [c for out in outs for c in out if c[4] > 0.1]
+
+    return outs
+
+
+
+
+
